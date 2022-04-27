3631a0fd024857aadd3e1664e0e6b0d8f720b078
DavidHorvitz <2205dzh@gmail.com>
2022-04-27 23:33:29 +0300

    1

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from './Layout.css';
+const Layout = (props) => (
+    <div>skyWWWWWWWWWWWWWWWWWWWWWWWWWWW</div>
+
+)
+
+
+;
+export default Layout;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Products = ()=>(
+    <div></div>
+);
+export default Products;
