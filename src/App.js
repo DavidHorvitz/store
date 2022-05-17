e4661db2905a20ff7497f1298c694ee440f36710
DavidHorvitz <2205dzh@gmail.com>
2022-05-18 00:29:35 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout/Layout';
+import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { getAllProducts } from './action/getData';
 
-function App() {
+// function App() {
+//   return (
+//     <div >
+//     <Layout/>
+//     </div>
+//   );
+// }
+
+// export default App;
+const App = props => {
+  useEffect(() => {
+    props.getAllProducts();
+  }, [])
   return (
-    <div >
-    <Layout/>
+    <div className="App">
+      <Layout />
+      
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {}
+}
+export default connect(mapStateToProps, { getAllProducts })(App);
--- src/components/Products/ProductBuilder/ProductBuilder.jsx
+++ src/components/Products/ProductBuilder/ProductBuilder.jsx
@@ -1,27 +1,40 @@
 import React from "react";
 import ProductCard from "../ProductCard/ProductCard";
-let productInformation_API = [
-    { productName: 'A', department: 'Store', price: 25, id: 3435, },
-    { productName: 'B', department: 'Store', price: 24, id: 34335, },
-    { productName: 'C', department: 'Store', price: 5, id: 345, },
-    { productName: 'D', department: 'Store', price: 4, id: 67874, },
-    { productName: 'E', department: 'Store', price: 1.5, id: 9784, },
-    { productName: 'F', department: 'Store', price: 1.4, id: 9484, },
-    { productName: 'G', department: 'Store', price: 1.4, id: 4484, }
-];
+import { connect } from 'react-redux';
 
-const ProductBuilder = () => (
-    <div>
-        {productInformation_API.map(p_Api => (
-            <ProductCard
-                key={p_Api.id} productName={p_Api.productName} department={p_Api.department}
-                price={p_Api.price} id={p_Api.id} />
-        ))}
-    </div>
-);
+// let productInformation_API = [
+//     { productName: 'A', department: 'Store', price: 25, productId: 3435, },
+//     { productName: 'B', department: 'Store', price: 24, productId: 34335, },
+//     { productName: 'C', department: 'Store', price: 5, productId: 345, },
+//     { productName: 'D', department: 'Store', price: 4, productId: 67874, },
+//     { productName: 'E', department: 'Store', price: 1.5, productId: 9784, },
+//     { productName: 'F', department: 'Store', price: 1.4, productId: 9484, },
+//     { productName: 'G', department: 'Store', price: 1.4, productId: 4484, }
+// ];
+// productName
+// department
+// price
+// productId
+
+const ProductBuilder = props => {
+    return (
+        <div>
+            {props.productData.map((p_Api, index) => (
+                <ProductCard
+                    key={p_Api.id} productName={p_Api.productName} department={p_Api.department}
+                    price={p_Api.price} id={p_Api.id} />
+            ))}
+        </div>
+    );
+}
 
 
 
 
-export default ProductBuilder;
-
+// export default ProductBuilder;
+const mapStateToProps = state => {
+    return {
+        productData: state.productData
+    }
+}
+export default connect(mapStateToProps, {})(ProductBuilder);
